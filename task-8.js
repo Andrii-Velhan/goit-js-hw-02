768d2902d7ffadf4565d6ec65cdddc4c11a00c19
Andrii-Velhan <velganaa@gmail.com>
2020-08-22 20:16:48 +0300

    input task-8 data

--- task-8.js
+++ task-8.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function reduceArray(array) {
+  let total = 0;
+  // Write code under this line
+
+  return total;
+}
+
+console.log(reduceArray([1, 2, 3]));
+// 6
+
+console.log(reduceArray([-2, 0, 2]));
+// 0
+
+console.log(reduceArray([1, 2, 2.5]));
+// 5.5
