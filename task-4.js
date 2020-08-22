28359870445c2f892c757b43690224b57bcb5b56
Andrii-Velhan <velganaa@gmail.com>
2020-08-22 16:34:57 +0300

    task-5

--- task-4.js
+++ task-4.js
@@ -32,3 +32,15 @@ console.log(
 Если длина больше maxLength, то функция обрезает строку до размера maxLength символов
 и добавляет в конец строки троеточие ..., после чего возвращает укороченную версию.
 */
+
+/*
+Результатом вызова функции должна быть строка
+Результатом вызова функции должна быть пустая строка, если аргумент - пустая строка
+Для аргумента - строки 'Curabitur ligula sapien, tincidunt non.' результатом будет эта же строка
+Для строки 'Vestibulum facilisis, purus nec pulvinar iaculis.' результатом должна быть строка 'Vestibulum facilisis, purus nec pulvinar...'
+Для строки 'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.' результатом должна быть строка 'Nunc sed turpis. Curabitur a felis in nu...'
+Для строки 'Curabitur ligula sapien.' результатом будет эта же строка
+Ожидается использование метода '.length'
+Для строки 'Vestibulum facilisis, purus nec pulvinar iaculis.' и maxLength = 30 результатом должна быть строка 'Vestibulum facilisis, purus ne...'
+Код должен содержать функцию
+*/
--- task-5.js
+++ task-5.js
@@ -1,1 +1,43 @@
 'use strict';
+
+function checkForSpam(str) {
+  // Write code under this line
+  let items = str.toLowerCase();
+
+  console.log(items);
+  return items.includes('sale') || items.includes('spam');
+}
+
+//console.log(checkForSpam('Latest technology news')); // false
+
+//console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+//console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+/*
+Задача 2-5
+Функция предикат
+Функция предикат возвращает true или false
+
+Напиши функцию checkForSpam(message),
+принимающую 1 параметр message - строку.
+Функция проверяет ее на содержание слов spam и sale.
+Если нашли запрещенное слово то функция
+возвращает true, если запрещенных слов нет функция
+возвращает false. Слова в строке могут быть
+в произвольном регистре.
+*/
+
+/*
+Результатом вызова функции должен быть булеан
+Результатом вызова функции должна false, если аргумент - пустая строка
+Для аргумента - строки 'Latest technology news' результатом будет false
+Для аргумента - строки 'JavaScript weekly newsletter' результатом будет false
+Для аргумента - строки 'Get best sale offers now!' результатом будет true
+Для аргумента - строки '[SPAM] How to earn fast money?' результатом будет true
+Ожидается использование метода '.includes'
+Ожидается использование метода '.toLowerCase'
+Код должен содержать функцию
+*/
